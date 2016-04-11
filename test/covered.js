072de6bc81e4b8af5845f0cbee3c2ce2edefbbad
Jacob Blakely <jacob@codekirei.com>
2016-04-11 12:44:42 -0700

    update test case wording to reflect changed src

--- test/covered.js
+++ test/covered.js
@@ -2,5 +2,5 @@ import assert from 'assert'
 import covered from '../src/covered'
 
 exports.covered = {
-  'returns bar': () => assert.equal(covered, 'bar'),
+  'is bar': () => assert.equal(covered, 'bar'),
 }
